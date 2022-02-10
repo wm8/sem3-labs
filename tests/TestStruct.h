9a68219116c9bf9aec4b31886ee86c363b1f990d
BuildTools <unconfigured@null.spigotmc.org>
2022-02-10 12:09:01 +0300

    build v1.7

--- tests/TestStruct.h
+++ tests/TestStruct.h
@@ -4,13 +4,14 @@
 #define LAB5_TESTSTRUCT_H
 struct TestStruct
 {
-  TestStruct() {}
+  TestStruct()
+  {
+    id=0;
+  }
   explicit TestStruct(int _id) {id = _id;}
   TestStruct(TestStruct* pStruct) {
     this->id = pStruct->id;
-    this->name = pStruct->name;
   }
   int id;
-  char* name= nullptr;
 };
 #endif  // LAB5_TESTSTRUCT_H
