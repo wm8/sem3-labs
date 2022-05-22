43c6e0815f7fa94a0ee22c2c262b612d869c77bd
BuildTools <unconfigured@null.spigotmc.org>
2022-05-22 16:24:09 +0300

    hello world

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,10 @@
-#include <example.hpp>
+//Copyright 2022 wm8
+#include "Utils.h"
+#include "DatabaseEditor.h"
+int main([[maybe_unused]] int argc,[[maybe_unused]]  char **argv) {
+  run(argc, argv);
+  /*DatabaseEditor* editor = new DatabaseEditor("../db");
+  delete editor;*/
+  return EXIT_SUCCESS;
 
-int main() {
-  example();
 }
